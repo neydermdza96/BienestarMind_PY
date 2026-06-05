43b24466a5fff3956cf9c93076636909609947c9
Neyder Mendoza <neydermdza96@gmail.com>
2026-06-04 21:32:07 -0500

    Update settings.py

--- bienestarmind/settings.py
+++ bienestarmind/settings.py
@@ -147,8 +147,9 @@ LOGOUT_REDIRECT_URL = '/usuarios/login/'
 # ── Email ──────────────────────────────────────────────────────────────────
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = 'BienestarMind SENA <noreply@bienestarmind.sena.edu.co>'
